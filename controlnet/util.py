78ec9d00b7d99817cc24fdb8c872adc3def4c7a1
Wotah Man <z.p.a.toto@gmail.com>
2024-08-31 15:18:09 +1000

    Update util.py
    Support basic image inpainting

--- controlnet/util.py
+++ controlnet/util.py
@@ -61,3 +61,42 @@ def cnet_txt2img(prompt, ref_img, batch_size=1, module='canny',url='http://127.0
         output_images.append(decode_base64_to_image(response.json()['images'][i]))
     
     return output_images
+
+
+def img2img_inpaint(prompt, img, mask, batch_size=1, url='http://127.0.0.1:7860'):
+    base64_img = encode_pil_to_base64(img)
+    base64_mask = encode_pil_to_base64(mask)
+    
+    img2img_value = {
+        'prompt': prompt,
+        'negative_prompt': 'lowres, blurry, bad anatomy, bad hands, cropped, worst quality',
+        'init_images': [base64_img],
+        'mask': base64_mask,
+        'sampler_name': 'DPM++ 2M Automatic',
+        'batch_size': batch_size,
+        'steps': 20,
+        'cfg_scale': 9,
+        'width': 768,   
+        'height': 768,
+        'denoising_strength': 0.75,     # low level = small change, vice versa
+        'image_cfg_scale':1.5,
+        'initial_noise_multiplier': 1,
+        'inpaint_full_res': 1,
+        'inpaint_full_res_padding': 32,
+        'inpainting_fill': 1,           # 1 = original, 0 = fill, 2 = latent noise, 3 = latent fill
+        'inpainting_mask_invert': 0,
+        'mask_blur': 10,
+        'mask_blur_x': 10,
+        'mask_blur_y': 10,
+        'mask_round': False,            # False = soft inpainting
+        'resize_mode': 1,
+        'alwayson_scripts': {}
+    }
+    
+    response = requests.post(url=f'{url}/sdapi/v1/img2img', json=img2img_value)
+    
+    output_images = []
+    for i in range(batch_size):
+        output_images.append(decode_base64_to_image(response.json()['images'][i]))
+    
+    return output_images
