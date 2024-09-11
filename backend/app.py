677367d5278ccccc7d60469313f71cefed46e780
Selinuntius02 <120080805+Selinuntius02@users.noreply.github.com>
2024-09-11 17:00:56 +1000

    corrected version of search()

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request
-from flask_cors import CORS
+from flask import Flask, request
+from flask_cors import CORS, cross_origin
 from search_engine_access import search_pinterest, response_pull_images
 from controlnet.util import cnet_txt2img
 from PIL import Image
@@ -9,40 +9,42 @@ import os
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/')
-def home():
-    return render_template('home.html')
-
-@app.route('/search', methods = ['GET', 'POST'])
+@app.route('/search', methods = ['POST'])
+@cross_origin()
 def search():
     if request.method == 'POST':
-        keyword = request.form['search']
+        keyword = request.get_json().get('query')
         raw_imgs = search_pinterest(keyword)
         images = response_pull_images(raw_imgs)
-        return render_template('search_results.html', images = images)
+        response = dict()
+        for i in range(len(images)):
+            response[i] = images[i]
+        return response
     else:
-        return render_template('search.html')
+        return {}
     
-@app.route('/generation', methods = ['GET', 'POST'])
-def generate():
-    if request.method == 'POST':
+@app.route('/upload', methods = ['GET', 'POST'])
+@cross_origin()
+def upload():
+    return request.form
+    # if request.method == 'POST':
 
-        prompt = request.form['prompt']
-        reference = request.files['reference']
+    #     prompt = request.form['prompt']
+    #     reference = request.files['reference']
 
-        path = os.path.join(app.root_path, 'images', reference.filename)
+    #     path = os.path.join(app.root_path, 'images', reference.filename)
 
-        reference.save(path)
+    #     reference.save(path)
 
-        ref_img = Image.open(path)
+    #     ref_img = Image.open(path)
 
-        images = cnet_txt2img(prompt, ref_img)
+    #     images = cnet_txt2img(prompt, ref_img)
 
-        # This section is a compromise as behaviour is unpredictable
-        return images
+    #     # This section is a compromise as behaviour is unpredictable
+    #     return images
 
-    else:
-        return render_template('generate.html')
+    # else:
+    #     return render_template('generate.html')
 if __name__ == '__main__':
 
     app.run(host = 'localhost', debug = True)
