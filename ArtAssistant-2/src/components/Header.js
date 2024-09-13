d103dd6885e9b81042e034b90161857053459daa
yxiao66666 <130287395+yxiao66666@users.noreply.github.com>
2024-09-13 10:21:35 +1000

    Moved inline styling into Header.css, deleted error line in About.js

--- ArtAssistant-2/src/components/Header.js
+++ ArtAssistant-2/src/components/Header.js
@@ -19,12 +19,12 @@ export default function Header() {
 
     return (
         <header>
-            <nav className="navbar navbar-expand-lg" style={{ backgroundColor: 'black' }}>
+            <nav className="navbar navbar-expand-lg custom-navbar">
                 <div className="container">
                     <Link 
-                        className="navbar-brand" 
-                        to="/" 
-                        style={{ color: 'white', fontSize: '22px', fontFamily: 'fantasy' }}
+                        className="navbar-brand brand-name" 
+                        style={{ color: 'white', transition: 'transform 0.3s', paddingLeft:'30px', paddingRight:'30px'}}
+                        to="/"
                     >
                         Arty
                     </Link>
@@ -43,44 +43,44 @@ export default function Header() {
                         <ul className="navbar-nav ms-auto me-auto">
                             <li className="nav-item">
                                 <Link 
-                                    className={`nav-link ${activeLink === "/" ? "active" : ""}`} 
-                                    to="/" 
-                                    style={{ color: 'white', transition: 'transform 0.3s', paddingRight:'30px'}}
+                                    className={`nav-link ${activeLink === "/" ? "active" : ""} nav-hover`} 
+                                    style={{ color: 'white', transition: 'transform 0.3s', paddingLeft:'30px', paddingRight:'30px'}}
                                     onMouseEnter={(e) => e.currentTarget.style.transform = 'scale(1.05)'}
                                     onMouseLeave={(e) => e.currentTarget.style.transform = 'scale(1)'}
+                                    to="/" 
                                 >
                                     Home
                                 </Link>
                             </li>
                             <li className="nav-item">
                                 <Link 
-                                    className={`nav-link ${activeLink === "/about" ? "active" : ""}`} 
-                                    to="/about" 
+                                    className={`nav-link ${activeLink === "/about" ? "active" : ""} nav-hover`} 
                                     style={{ color: 'white', transition: 'transform 0.3s', paddingLeft:'30px', paddingRight:'30px'}}
                                     onMouseEnter={(e) => e.currentTarget.style.transform = 'scale(1.05)'}
                                     onMouseLeave={(e) => e.currentTarget.style.transform = 'scale(1)'}
+                                    to="/about" 
                                 >
                                     About
                                 </Link>
                             </li>
                             <li className="nav-item">
                                 <Link 
-                                    className={`nav-link ${activeLink === "/search" ? "active" : ""}`} 
-                                    to="/search" 
+                                    className={`nav-link ${activeLink === "/search" ? "active" : ""} nav-hover`} 
                                     style={{ color: 'white', transition: 'transform 0.3s', paddingLeft:'30px', paddingRight:'30px'}}
                                     onMouseEnter={(e) => e.currentTarget.style.transform = 'scale(1.05)'}
                                     onMouseLeave={(e) => e.currentTarget.style.transform = 'scale(1)'}
+                                    to="/search"
                                 >
                                     Search
                                 </Link>
                             </li>
                             <li className="nav-item">
                                 <Link 
-                                    className={`nav-link ${activeLink === "/upload" ? "active" : ""}`} 
-                                    to="/upload" 
+                                    className={`nav-link ${activeLink === "/upload" ? "active" : ""} nav-hover`} 
                                     style={{ color: 'white', transition: 'transform 0.3s', paddingLeft:'30px', paddingRight:'30px'}}
                                     onMouseEnter={(e) => e.currentTarget.style.transform = 'scale(1.05)'}
                                     onMouseLeave={(e) => e.currentTarget.style.transform = 'scale(1)'}
+                                    to="/upload"
                                 >
                                     Generator
                                 </Link>
@@ -91,18 +91,19 @@ export default function Header() {
                             <li className="nav-item">
                                 {isLoggedIn ? (
                                     <Link 
-                                        className={`nav-link ${activeLink === "/user" ? "active" : ""}`} 
-                                        to="/user" 
+                                        className={`nav-link ${activeLink === "/user" ? "active" : ""} nav-hover`} 
                                         style={{ color: 'white', transition: 'transform 0.3s', paddingLeft:'30px', paddingRight:'30px'}}
                                         onMouseEnter={(e) => e.currentTarget.style.transform = 'scale(1.05)'}
                                         onMouseLeave={(e) => e.currentTarget.style.transform = 'scale(1)'}
+                                        to="/user"
                                     >Account</Link>
                                 ) : (
                                     <Link 
-                                        className={`nav-link ${activeLink === "/" ? "active" : ""}`} 
-                                        style={{ color: 'white',transition: 'transform 0.3s', paddingLeft:'30px', paddingRight:'30px' }}
+                                        className={`nav-link ${activeLink === "/" ? "active" : ""} nav-hover`} 
+                                        style={{ color: 'white', transition: 'transform 0.3s', paddingLeft:'30px', paddingRight:'30px'}}
                                         onMouseEnter={(e) => e.currentTarget.style.transform = 'scale(1.05)'}
                                         onMouseLeave={(e) => e.currentTarget.style.transform = 'scale(1)'}
+                                        to="/"
                                         onClick={(e) => {
                                             e.preventDefault();
                                             setShowPopup(true);
@@ -153,112 +154,59 @@ const LoginSignupPopup = ({ closePopup, onLogin }) => {
         }
     };
 
-    const inputStyle = {
-        width: '100%',
-        border: 'none',
-        borderBottom: '1px solid gray',
-        padding: '8px 0',
-        outline: 'none',
-        fontSize: '1em'
-    };
-
     return (
-        <div style={popupStyles.overlay} onClick={handleOverlayClick}>
-            <div style={popupStyles.popup}>
+        <div className="popup-overlay" onClick={handleOverlayClick}>
+            <div className="popup-container">
                 {isLogin ? (
                     <>
-                        <form onSubmit={handleSubmit} style={{ width: '100%', maxWidth: '600px', margin: '40px auto' }}>
-                        <h4 style={{ textAlign: 'center' }}>Welcome</h4>
+                        <form onSubmit={handleSubmit} className="form-container">
+                            <h4>Welcome</h4>
+                            <label>
+                                Email
+                                <input type="email" name="email" required className="input-field" />
+                            </label>
+                            <label>
+                                Password
+                                <input type="password" name="password" required className="input-field" />
+                            </label>
                             <br />
-                            <label style={{ display: 'block', textAlign: 'left', width: '100%' ,fontSize:'0.9em'}}>
-                                Email<input type="email" name="email" required style={inputStyle} />
-                            </label>
-                            <br /><br />
-                            <label style={{ display: 'block', textAlign: 'left', width: '100%',fontSize:'0.9em' }}>
-                                Password<input type="password" name="password" required style={inputStyle} />
-                            </label><br />
-                            <label style={{ display: 'flex', alignItems: 'center' , fontSize:'0.7em'}}>
-                                <input type="checkbox" name="rememberMe" style={{ marginRight: '10px' }} />
-                                Remember me
-                            </label>
                             <br />
                             <button 
                                 type="submit" 
-                                style={{
-                                    padding: '5px 10px', 
-                                    width: '100%', 
-                                    border: '1px solid blue', 
-                                    borderRadius: '5px',
-                                    backgroundColor: 'blue', 
-                                    color: 'white',           
-                                    fontWeight: 'bold',
-                                    transition: 'background-color 0.3s, transform 0.3s'
-                                }}
-                                onMouseEnter={(e) => {
-                                    e.currentTarget.style.backgroundColor = 'darkblue';
-                                    e.currentTarget.style.border = '1px solid darkblue';
-                                }}
-                                onMouseLeave={(e) => {
-                                    e.currentTarget.style.backgroundColor = 'blue';  
-                                    e.currentTarget.style.border = '1px solid blue';
-                                }}
+                                className="btn-primary"
                             >LOGIN</button>
                         </form>
-                        <br /><br />
-                        <span style={{ fontSize:'0.7em', display: 'block', textAlign: 'center'}}>
+                        <span className="form-footer">
                             Don't have an account? 
-                            <button 
-                                type="button" 
-                                onClick={toggleForm}
-                                style={{ background: 'none', color: 'blue', border: 'none', cursor: 'pointer', fontWeight: 'bold' }}
-                            >Sign up</button>
+                            <button onClick={toggleForm} className="form-toggle">Sign up</button>
                         </span>
                     </>
                 ) : (
                     <>
-                        
-                        <form onSubmit={handleSubmit} style={{ width: '100%', maxWidth: '600px', margin: '40px auto' }}>
-                        <h4 style={{ textAlign: 'center' }}>Create an Account</h4>
-                        <br />
-                            <label style={{ display: 'block', textAlign: 'left', width: '100%',fontSize:'0.9em' }}>
-                                Email<input type="email" name="email" required style={inputStyle} />
-                            </label><br /><br />
-                            <label style={{ display: 'block', textAlign: 'left', width: '100%',fontSize:'0.9em' }}>
-                                Password<input type="password" name="password" required style={inputStyle} />
-                            </label><br /><br />
-                            <label style={{ display: 'block', textAlign: 'left', width: '100%',fontSize:'0.9em' }}>
-                                Confirm Password<input type="password" name="confirmPassword" required style={inputStyle} />
-                            </label><br /><br />
+                        <form onSubmit={handleSubmit} className="form-container">
+                            <h4>Create an Account</h4>
+                            <label>
+                                Email
+                                <input type="email" name="email" required className="input-field" />
+                            </label>
+                            <label>
+                                Password
+                                <input type="password" name="password" required className="input-field" />
+                            </label>
+                            <label>
+                                Confirm Password
+                                <input type="password" name="confirmPassword" required className="input-field" />
+                            </label>
+                            <br />
+                            <br />
                             <button 
                                 type="submit" 
-                                style={{
-                                    padding: '5px 10px', 
-                                    width: '100%', 
-                                    border: '1px solid blue', 
-                                    borderRadius: '5px',
-                                    backgroundColor: 'blue', 
-                                    color: 'white',
-                                    fontWeight: 'bold',
-                                    transition: 'background-color 0.3s, transform 0.3s'
-                                }}
-                                onMouseEnter={(e) => {
-                                    e.currentTarget.style.backgroundColor = 'darkblue';
-                                    e.currentTarget.style.border = '1px solid darkblue';
-                                }}
-                                onMouseLeave={(e) => {
-                                    e.currentTarget.style.backgroundColor = 'blue';  
-                                    e.currentTarget.style.border = '1px solid blue';
-                                }}
+                                className="btn-primary"
                             >SIGN UP</button>
                         </form>
-                        <br />
-                        <span style={{ fontSize:'0.7em', display: 'block', textAlign: 'center'}}>
+                        <span className="form-footer">
                             Already have an account? 
-                            <button 
-                                type="button" 
-                                onClick={toggleForm}
-                                style={{ background: 'none', color: 'blue', border: 'none', cursor: 'pointer', fontWeight: 'bold' }}
-                            >Login</button>
+                            <button onClick={toggleForm} className="form-toggle">Login</button>
                         </span>
                     </>
                 )}
@@ -267,25 +215,4 @@ const LoginSignupPopup = ({ closePopup, onLogin }) => {
     );
 };
 
-const popupStyles = {
-    overlay: {
-        position: 'fixed',
-        top: 0,
-        left: 0,
-        width: '100%',
-        height: '100%',
-        backgroundColor: 'rgba(0, 0, 0, 0.5)',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        zIndex: 1000,
-    },
-    popup: {
-        backgroundColor: 'white',
-        padding: '40px',
-        borderRadius: '8px',
-        width: '90%',
-        maxWidth: '500px',
-        position: 'relative',
-    },
-};
+
--- ArtAssistant-2/src/pages/About.js
+++ ArtAssistant-2/src/pages/About.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "../styles/About.css";
-import william from './app-icon.png'; 
 
 // About Us page
 export default function About() {
