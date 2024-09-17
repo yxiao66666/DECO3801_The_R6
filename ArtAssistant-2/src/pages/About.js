0de358df928335c4a99a7e5b63999cdf91637599
yxiao66666 <130287395+yxiao66666@users.noreply.github.com>
2024-09-17 18:06:15 +1000

    Moved inline styles into css files

--- ArtAssistant-2/src/pages/About.js
+++ ArtAssistant-2/src/pages/About.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "../styles/About.css";
-import william from './app-icon.png'; 
 
 // About Us page
 export default function About() {
@@ -9,15 +8,15 @@ export default function About() {
         <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
     
         {/* About Us */}
-        <div className="about-us-block" style={{ backgroundImage: 'url(/images/about_bg.JPG)', backgroundSize: 'cover', backgroundPosition: 'center', padding: '230px 0', textAlign: 'center' }}>
+        <div className="about-us-block" style={{ backgroundImage: 'url(/images/about_bg.JPG)' }}>
             <div className="container">
                 <div className="row justify-content-center">
                     <div className="col-lg-8">
-                        <h1 style={{ color: 'white', fontSize: '4em', fontFamily: 'cursive', fontWeight: 'bold' }}>Nice to meet u</h1>
-                        <p style={{ color: 'white', fontSize: '1.5em', marginTop: '20px' }}>
+                        <h1 className="heading-nice-to-meet">Nice to meet u</h1>
+                        <p className="paragraph-introduction">
                             We are thrilled to introduce ourselves and share our passion for AI and art.
                         </p>
-                        <a href="#what-we-do" className="btn-learn-more" style={{ marginTop: '100px' }}>Learn More</a>
+                        <a href="#what-we-do" className="btn-learn-more">Learn More</a>
                     </div>
                 </div>
             </div>
@@ -25,25 +24,24 @@ export default function About() {
 
 
         {/* What We Do */}
-        <div id="what-we-do" className="what-we-do-block" style={{ backgroundColor: 'white', padding: '50px 0', marginTop: '50px' }}>
+        <div id="what-we-do" className="what-we-do-block">
             <div className="container">
                 <div className="row justify-content-center">
                     <div className="col-12 col-sm-8 col-lg-6">
                         <div className="section_heading text-center wow fadeInUp" 
-                            data-wow-delay="0.2s" 
-                            style={{ visibility: 'visible', animationDelay: '0.2s', animationName: 'fadeInUp' }}>
-                            <h2 style={{ fontFamily: 'cursive', fontSize: '4em', color: 'black', fontWeight: 'bold' }}>
+                            data-wow-delay="0.2s">
+                            <h2>
                                 What We Do
                             </h2>
-                            <img src="images/what we do.png" alt="Illustration" className="illustration" style={{  marginTop: '60px', maxWidth: '70%', height: 'auto' }} />
+                            <img src="images/what we do.png" alt="Illustration" className="illustration"/>
 
-                            <p style={{ fontSize: '1.5em', color: 'black', marginTop: '30px' }}>
+                            <p className="text-paragraph">
                                 Our platform is dedicated to providing innovative AI-driven tools for artists.
                             </p>
-                            <p style={{ fontSize: '1.5em', color: 'black', marginTop: '40px' }}>
+                            <p className="text-paragraph">
                                 We offer smart image generation, advanced style recognition, and seamless integration to enhance creativity and productivity.
                             </p>
-                            <p style={{ fontSize: '1.5em', color: 'black', marginTop: '40px' }}>
+                            <p className="text-paragraph">
                                 By leveraging cutting-edge technology, we aim to support artists in exploring new artistic possibilities and bringing their creative visions to life.
                             </p>
                         </div>
@@ -53,25 +51,24 @@ export default function About() {
         </div>
 
         {/* What you can do */}
-        <div className="what-you-can-do-block" style={{ backgroundColor: 'white', padding: '50px 0', marginTop: '50px' }}>
+        <div className="what-you-can-do-block">
             <div className="container">
                 <div className="row justify-content-center">
                     <div className="col-12 col-sm-8 col-lg-6">
                         <div className="section_heading text-center wow fadeInUp" 
-                            data-wow-delay="0.2s" 
-                            style={{ visibility: 'visible', animationDelay: '0.2s', animationName: 'fadeInUp' }}>
-                            <h2 style={{ fontFamily: 'cursive', fontSize: '4em', color: 'black', fontWeight: 'bold' }}>
+                            data-wow-delay="0.2s">
+                            <h2>
                                 What You Can Do
                             </h2>
                             <img src="images/what you can do.png" alt="Illustration" className="illustration" style={{  marginTop: '10px', maxWidth: '70%', height: 'auto' }} />
 
-                            <p style={{ fontSize: '1.5em', color: 'black', marginTop: '30px' }}>
+                            <p>
                             - Search for References: Enter text prompts to search for reference images that match your creative needs.
                             </p>
-                            <p style={{ fontSize: '1.5em', color: 'black', marginTop: '30px' }}>
+                            <p>
                             - Real-Time Reference Generation: Generate and arrange assets by inputting text or images to create reference images that align with your creative intentions.
                             </p>
-                            <p style={{ fontSize: '1.5em', color: 'black', marginTop: '30px' }}>
+                            <p>
                             - Cross-Device Usage: Use the software on desktop, laptop, or iPad to assist in their creative process.
                             </p>
                         </div>
@@ -80,20 +77,19 @@ export default function About() {
             </div>
         </div>
 
-
+        {/* Who we are */}
         <div className="container">
             <div className="row justify-content-center">
                 <div className="col-12 col-sm-8 col-lg-6">
                     <div className="section_heading text-center wow fadeInUp" 
-                        data-wow-delay="0.2s" 
-                        style={{ visibility: 'visible', animationDelay: '0.2s', animationName: 'fadeInUp' }}>
-                        <h3 style={{  marginTop: '100px', fontFamily:'cursive', fontSize: '4em', color: 'black', fontWeight: 'bold' }}>
+                        data-wow-delay="0.2s" >
+                        <h2>
                         Who We <span> Are</span>
-                        </h3>
-                        <p style={{ fontSize: '1.3em', color: 'black', marginTop: '50px'}}>
+                        </h2>
+                        <p className="who-we-are">
                         A team of innovators pushing the boundaries of art and technology.
                         </p>
-                        <p style={{ fontSize: '1.3em', color: 'black', marginTop: '30px',marginBottom: '80px'}}>
+                        <p className="who-we-are">
                         Our mission is to blend creativity with AI to empower artists and inspire new forms of expression.
                         </p>
                         <div className="line"></div>
@@ -103,10 +99,9 @@ export default function About() {
             <div className="row">
                 <div className="col-12 col-sm-6 col-lg-4">
                     <div className="single_advisor_profile wow fadeInUp" 
-                        data-wow-delay="0.2s" 
-                        style={{ visibility: 'visible', animationDelay: '0.2s', animationName: 'fadeInUp' }}>
+                        data-wow-delay="0.2s">
                         <div className="advisor_thumb"><img src="images/william.JPG" alt="" />
-                            <div className="social-info"><a href="#"><i className="fa fa-facebook"></i></a><a href="#"><i className="fa fa-twitter"></i></a><a href="#"><i className="fa fa-linkedin"></i></a></div>
+                            <div className="social-info"><a href="https://www.instagram.com/wmercado0"><i className="fa fa-instagram"></i></a><a href="https://www.instagram.com/wmercado0"><i className="fa fa-twitter"></i></a><a href="https://www.instagram.com/wmercado0"><i className="fa fa-linkedin"></i></a></div>
                         </div>
                         <div className="single_advisor_details_info">
                             <h6>William Mercado</h6>
@@ -116,11 +111,10 @@ export default function About() {
                 </div>
 
                 <div className="col-12 col-sm-6 col-lg-4">
-                    <div className="single_advisor_profile wow fadeInUp" 
-                        data-wow-delay="0.3s" 
-                        style={{ visibility: 'visible', animationDelay: '0.3s', animationName: 'fadeInUp' }}>
+                <div className="single_advisor_profile wow fadeInUp" 
+                        data-wow-delay="0.2s">
                         <div className="advisor_thumb"><img src="images/brian.JPG" alt="" />
-                            <div className="social-info"><a href="#"><i className="fa fa-facebook"></i></a><a href="#"><i className="fa fa-twitter"></i></a><a href="#"><i className="fa fa-linkedin"></i></a></div>
+                            <div className="social-info"><a href="https://www.instagram.com/z.pengfei_"><i className="fa fa-instagram"></i></a><a href="https://www.instagram.com/z.pengfei_"><i className="fa fa-twitter"></i></a><a href="https://www.instagram.com/z.pengfei_"><i className="fa fa-linkedin"></i></a></div>
                         </div>
                         <div className="single_advisor_details_info">
                             <h6>Brian Zhang</h6>
@@ -130,11 +124,10 @@ export default function About() {
                 </div>
 
                 <div className="col-12 col-sm-6 col-lg-4">
-                    <div className="single_advisor_profile wow fadeInUp" 
-                        data-wow-delay="0.4s" 
-                        style={{ visibility: 'visible', animationDelay: '0.4s', animationName: 'fadeInUp' }}>
+                <div className="single_advisor_profile wow fadeInUp" 
+                        data-wow-delay="0.2s">
                         <div className="advisor_thumb"><img src="images/ryuto.PNG" alt="" />
-                            <div className="social-info"><a href="#"><i className="fa fa-facebook"></i></a><a href="#"><i className="fa fa-twitter"></i></a><a href="#"><i className="fa fa-linkedin"></i></a></div>
+                            <div className="social-info"><a href="https://www.instagram.com/hisattohisahisa"><i className="fa fa-instagram"></i></a><a href="https://www.instagram.com/hisattohisahisa"><i className="fa fa-twitter"></i></a><a href="https://www.instagram.com/hisattohisahisa"><i className="fa fa-linkedin"></i></a></div>
                         </div>
                         <div className="single_advisor_details_info">
                             <h6>Ryuto Hisamoto</h6>
@@ -144,11 +137,10 @@ export default function About() {
                 </div>
 
                 <div className="col-12 col-sm-6 col-lg-4">
-                    <div className="single_advisor_profile wow fadeInUp" 
-                        data-wow-delay="0.5s" 
-                        style={{ visibility: 'visible', animationDelay: '0.5s', animationName: 'fadeInUp' }}>
+                <div className="single_advisor_profile wow fadeInUp" 
+                        data-wow-delay="0.2s">
                         <div className="advisor_thumb"><img src="images/yang.JPG" alt="" />
-                            <div className="social-info"><a href="#"><i className="fa fa-facebook"></i></a><a href="#"><i className="fa fa-twitter"></i></a><a href="#"><i className="fa fa-linkedin"></i></a></div>
+                            <div className="social-info"><a href="https://www.instagram.com/y.a.n.g_official"><i className="fa fa-instagram"></i></a><a href="https://www.instagram.com/y.a.n.g_official"><i className="fa fa-twitter"></i></a><a href="https://www.instagram.com/y.a.n.g_official"><i className="fa fa-linkedin"></i></a></div>
                         </div>
                         <div className="single_advisor_details_info">
                             <h6>Yang Xiao</h6>
@@ -158,11 +150,10 @@ export default function About() {
                 </div>
 
                 <div className="col-12 col-sm-6 col-lg-4">
-                    <div className="single_advisor_profile wow fadeInUp" 
-                        data-wow-delay="0.6s" 
-                        style={{ visibility: 'visible', animationDelay: '0.6s', animationName: 'fadeInUp' }}>
+                <div className="single_advisor_profile wow fadeInUp" 
+                        data-wow-delay="0.2s">
                         <div className="advisor_thumb"><img src="images/shan.jpeg" alt="" />
-                            <div className="social-info"><a href="#"><i className="fa fa-facebook"></i></a><a href="#"><i className="fa fa-twitter"></i></a><a href="#"><i className="fa fa-linkedin"></i></a></div>
+                            <div className="social-info"><a href="https://www.instagram.com/cecili_l"><i className="fa fa-instagram"></i></a><a href="https://www.instagram.com/cecili_l"><i className="fa fa-twitter"></i></a><a href="https://www.instagram.com/cecili_l"><i className="fa fa-linkedin"></i></a></div>
                         </div>
                         <div className="single_advisor_details_info">
                             <h6>Shan Liu</h6>
@@ -172,11 +163,10 @@ export default function About() {
                 </div>
 
                 <div className="col-12 col-sm-6 col-lg-4">
-                    <div className="single_advisor_profile wow fadeInUp" 
-                        data-wow-delay="0.7s" 
-                        style={{ visibility: 'visible', animationDelay: '0.7s', animationName: 'fadeInUp' }}>
+                <div className="single_advisor_profile wow fadeInUp" 
+                        data-wow-delay="0.2s">
                         <div className="advisor_thumb"><img src="images/hongyingzi.jpg" alt="" />
-                            <div className="social-info"><a href="#"><i className="fa fa-facebook"></i></a><a href="#"><i className="fa fa-twitter"></i></a><a href="#"><i className="fa fa-linkedin"></i></a></div>
+                            <div className="social-info"><a href="https://www.instagram.com/olivialuuuuuu"><i className="fa fa-instagram"></i></a><a href="https://www.instagram.com/olivialuuuuuu"><i className="fa fa-twitter"></i></a><a href="https://www.instagram.com/olivialuuuuuu"><i className="fa fa-linkedin"></i></a></div>
                         </div>
                         <div className="single_advisor_details_info">
                             <h6>Hongyingzi Lu</h6>
