530adb2a1dfc1732bb842f05948da541516987f0
yxiao66666 <130287395+yxiao66666@users.noreply.github.com>
2024-10-09 17:21:03 +1000

    A animated save button not working yet

--- frontend/src/pages/User.js
+++ frontend/src/pages/User.js
@@ -12,6 +12,8 @@ export default function User() {
     const baseUrl = 'http://127.0.0.1:5000'; // Define the base URL for the backend API requests
     const [visibleImages] = useState(999); // To control how many images are visible initially
 
+
+
     useEffect(() => {
         const id = localStorage.getItem('userId');
         setUserId(id); // Fetch id
@@ -333,6 +335,22 @@ export default function User() {
                                         className="save-icon" 
                                         onClick={() => toggleSaveGeneratedImage(url)} 
                                     />
+                                    <button className="like-button" onClick={() => toggleSaveGeneratedImage(url)}>
+                                        <div className="like-wrapper">
+                                            <div className="ripple"></div>
+                                            <svg className="heart" style={{width:"24", height:"24", viewBox:"0 0 24 24"}}>
+                                                <path d="M12,21.35L10.55,20.03C5.4,15.36 2,12.27 2,8.5C2,5.41 4.42,3 7.5,3C9.24,3 10.91,3.81 12,5.08C13.09,3.81 14.76,3 16.5,3C19.58,3 22,5.41 22,8.5C22,12.27 18.6,15.36 13.45,20.03L12,21.35Z"></path>
+                                            </svg>
+                                            <div className="particles" style={{ '--total-particles': 6 }}>
+                                                <div className="particle" style={{ '--i': 1, '--color': '#7642F0' }}></div>
+                                                <div className="particle" style={{ '--i': 2, '--color': '#AFD27F' }}></div>
+                                                <div className="particle" style={{ '--i': 3, '--color': '#DE8F4F' }}></div>
+                                                <div className="particle" style={{ '--i': 4, '--color': '#D0516B' }}></div>
+                                                <div className="particle" style={{ '--i': 5, '--color': '#5686F2' }}></div>
+                                                <div className="particle" style={{ '--i': 6, '--color': '#D53EF3' }}></div>
+                                            </div>
+                                        </div>
+                                    </button>
                                 </div>
                             ))
                         ) : (
