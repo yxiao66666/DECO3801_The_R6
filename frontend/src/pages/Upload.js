37135555dc182485d9e0d41733cae55bb481a270
yxiao66666 <130287395+yxiao66666@users.noreply.github.com>
2024-10-09 22:38:26 +1000

    Save generated image button fully working

--- frontend/src/pages/Upload.js
+++ frontend/src/pages/Upload.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useRef, useCallback } from "react";
 import "../styles/Upload.css";
+import "../styles/Button.css"; 
 
 export default function Upload() {
     const [files, setFiles] = useState([]);  // Array of selected files
@@ -754,13 +755,29 @@ export default function Upload() {
                                     src={`${baseUrl}/static/generations/${imageName}`}
                                     alt={`Generated ${index}`}
                                 />
-                                {/* Show a save or saved icon based on whether the image is saved */}
-                                <img 
-                                    src={savedGeneratedImages.has(imageName) ? "../images/saved.png" : "../images/save.png"} 
-                                    alt={savedGeneratedImages.has(imageName) ? "Saved Icon" : "Save Icon"} 
-                                    className="save-icon" 
-                                    onClick={() => toggleSaveGeneratedImage(imageName)} 
-                                />
+                                {/* Save button */}
+                                <button
+                                    className={`like-button ${savedGeneratedImages.has(imageName) ? 'liked' : ''}`}
+                                    onClick={() => {
+                                        toggleSaveGeneratedImage(imageName);
+                                    }}
+                                >
+                                    <div className="like-wrapper">
+                                        <div className="ripple"></div>
+                                        <svg className="heart" style={{ width: '24', height: '24', viewBox: '0 0 24 24' }}>
+                                            <path d="M12,21.35L10.55,20.03C5.4,15.36 2,12.27 2,8.5C2,5.41 4.42,3 7.5,3C9.24,3 10.91,3.81 12,5.08C13.09,3.81 14.76,3 16.5,3C19.58,3 22,5.41 22,8.5C22,12.27 18.6,15.36 13.45,20.03L12,21.35Z"></path>
+                                        </svg>
+                                        <div className="particles" style={{ '--total-particles': 6 }}>
+                                            <div className="particle" style={{ '--i': 1, '--color': '#7642F0' }}></div>
+                                            <div className="particle" style={{ '--i': 2, '--color': '#AFD27F' }}></div>
+                                            <div className="particle" style={{ '--i': 3, '--color': '#DE8F4F' }}></div>
+                                            <div className="particle" style={{ '--i': 4, '--color': '#D0516B' }}></div>
+                                            <div className="particle" style={{ '--i': 5, '--color': '#5686F2' }}></div>
+                                            <div className="particle" style={{ '--i': 6, '--color': '#D53EF3' }}></div>
+                                        </div>
+                                    </div>
+                                </button>
+
                             </div>
                         ))}
                     </div>
